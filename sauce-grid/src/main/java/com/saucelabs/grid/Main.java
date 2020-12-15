de4f55b7f58c97e7f1798f26f40067e59d44d314
Diego Molina <diemol@gmail.com>
2020-12-16 00:14:27 +0100

    Removing hardcoded paths for debugging

--- sauce-grid/src/main/java/com/saucelabs/grid/Main.java
+++ sauce-grid/src/main/java/com/saucelabs/grid/Main.java
@@ -1,19 +1,31 @@
 package com.saucelabs.grid;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
   public static void main(String[] args) {
+    // Run "mvn clean package" before running this
+    Path jarPath = Paths.get("sauce-grid/target/sauce-grid-1.0-SNAPSHOT.jar").toAbsolutePath();
+    // Use the same "sauce_togo_config.toml" located under "test/resources" and add the following
+    // two lines
+    // Path where test assets will be stored (this path must exist, use ONLY for code debugging)
+    // assets-path = "/absolute/path/to/your/assets/directory"
+    Path debugToml =
+      Paths
+        .get("sauce-grid/src/test/resources/sauce_togo_config_debugging.toml")
+        .toAbsolutePath();
     org.openqa.selenium.grid.Main.main(
       new String[]{
         "--ext",
-        // TODO Use resource loader
-        "/Users/diegomolina/Projects/github.com/saucelabs/sauce-togo/sauce-grid/target/sauce-grid-1.0-SNAPSHOT.jar",
+        jarPath.toString(),
         "standalone",
         "--relax-checks",
         "true",
         "--detect-drivers",
         "false",
         "--config",
-        "/Users/diegomolina/Projects/github.com/seleniumhq/config_host_sauce.toml"
+        debugToml.toString()
       }
     );
   }
